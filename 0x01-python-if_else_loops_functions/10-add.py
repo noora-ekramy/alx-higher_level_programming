048807720cdc4d47596e66464dfe2b34d93c01a6
Nora Ekramy <nora.ekramy11@gmail.com>
2023-10-04 04:07:43 +0300

    mandatory

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Nora Ekramy
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Nora Ekramy
+
+def pow(a, b):
+    return (a ** b)
