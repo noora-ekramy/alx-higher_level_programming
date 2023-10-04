90817e0b7ae35592d429a7853ee4934cdb5466a0
Nora <nora.ekramy11@gmail.com>
2023-10-03 18:26:47 -0700

    100

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author -Nora Ekramy
+
+for i in range(26):
+    if(i % 2):
+        print("{}".format(chr(ord('Z') - i)) , end="")
+    else:
+        print("{}".format(chr(ord('z') - i)) , end="")
