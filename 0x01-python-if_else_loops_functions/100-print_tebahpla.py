15539b47e037cecb5e63a590bb2e94634bb7b7d2
Nora Ekramy <nora.ekramy11@gmail.com>
2023-10-04 04:32:54 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 # Author -Nora Ekramy
 
 for i in range(26):
-    if(i % 2):
-        print("{}".format(chr(ord('Z') - i)) , end="")
+    if i % 2:
+        print("{}".format(chr(ord('Z') - i)), end="")
     else:
-        print("{}".format(chr(ord('z') - i)) , end="")
+        print("{}".format(chr(ord('z') - i)), end="")
